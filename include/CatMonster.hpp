6874198d5b1cdd065aa82a6bf91cf7a71ff12216
EvaBien <eva.benharira@gmail.com>
2019-05-05 22:54:08 +0200

    edit catMonster.cpp add methods

--- include/CatMonster.hpp
+++ include/CatMonster.hpp
@@ -1,11 +1,23 @@
 #include <string>
 
 class CatMonster{
-    //Attributs
+    // Attributs
     string m_typeCat;
     int m_life;
     int m_defense;
     int m_gainDeath;
     int m_speed;
 
+    // Méthodes
+    void beDamaged(){
+
+    }
+
+    void move(){
+        
+    }
+
+    bool isAlive(){
+
+    }
 }
