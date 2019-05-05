7daba04706b8753fe9295ddf84d769fcefe9ab69
EvaBien <eva.benharira@gmail.com>
2019-05-05 22:36:07 +0200

    edit CatMonster files and add attributs

---
+++ include/CatMonster.hpp
@@ -0,0 +1,11 @@
+#include <string>
+
+class CatMonster{
+    //Attributs
+    string m_typeCat;
+    int m_life;
+    int m_defense;
+    int m_gainDeath;
+    int m_speed;
+
+}
--- include/Cat_monster.hpp
+++
@@ -1,7 +0,0 @@
-class Cat_monster{
-    string m_typeCat;
-    int m_life;
-    int m_defense;
-    int gainDeath;
-    int speed;
-}
