f8397d2d003fd8a581f14113a176ed9befe5da4e
EvaBien <eva.benharira@gmail.com>
2019-05-05 22:26:23 +0200

    Add basic files for Cat_monster objects

---
+++ include/Cat_monster.hpp
@@ -0,0 +1,7 @@
+class Cat_monster{
+    string m_typeCat;
+    int m_life;
+    int m_defense;
+    int gainDeath;
+    int speed;
+}
---
+++ src/Cat_monster.cpp
@@ -0,0 +1,5 @@
+#include Cat_monster.hpp
+
+Cat_monster {
+    
+}
