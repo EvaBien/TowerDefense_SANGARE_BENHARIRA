c996f9215c6ce7258dec99cc645c24a700b0eae4
EvaBien <eva.benharira@gmail.com>
2019-05-09 16:04:52 +0200

    Tower construct + init towerType getter and setter

--- src/Tower.cpp
+++ src/Tower.cpp
@@ -4,15 +4,27 @@
 
 using namespace std;
 
-Tower (Type_tower type){
+Tower (TowerType type){ //on considère 100 comme valeur neutre
     if (type == RED_LASER){
-
+        m_damages = 150; //bcp de dégâts
+        m_portee = 100;
+        m_cadence = 50; //cadence faible
+        m_price = 100;
     } else if (type == GREEN_GRASS) {
-
-    } else if (type == YELLOW_GAMMELLE) {
-
+        m_damages = 100; // dégâts moyens
+        m_portee = 50; //faible portée
+        m_cadence = 150; //tirent très rapidement
+        m_price = 100;
+    } else if (type == YELLOW_GAMMELLE) { //tirents sur tous les ennemis à leur portée
+        m_damages = 50; //peu de dégâts
+        m_portee = 50; //portée très limitée
+        m_cadence = 125; //bonne cadence de tir
+        m_price = 100;
     } else { // ie BLUE_MILK 
-
+        m_damages = 50; //peu de dégâts
+        m_portee =150 ; //bonne portée
+        m_cadence = 150; //bonne cadence de tir
+        m_price = 100;
     }
 }
 
@@ -37,6 +49,11 @@ int getPrice(){
     return m_price;
 }
 
+int getTowerType(){
+    TowerType type = this->type;
+    return type;
+}
+
 /////////SETTERS//////////
 void setDamages(int damages){
     this->m_damages =  damages; //Remplace valeur damages par une nouvelle
@@ -52,6 +69,10 @@ void setCadence(int cadence){
 
 void setPrice(int price){
     this->m_price =  price;
+}
+
+void setTowerType(towerType){
+    this->type = towerType;
 }
 
 //////////OTHER METHODS//////////
