0b3e5bcb2a63eac8b047ac001583299a8238812d
EvaBien <eva.benharira@gmail.com>
2019-05-23 16:16:11 +0200

    Map files structure and research on scaling

--- include/Map.hpp
+++ include/Map.hpp
@@ -7,17 +7,23 @@
 class Map{
     private:
     // Attributs
+    int m_height;
+    int m_width;
 
     public:
 
     Map(); //constructeur
 
     //////GETTERS/////
+    int getHeight();
+    int getWidth();
 
     //////SETTERS/////
-
+    int setHeight();
+    int setWidth();
+    
     //////OTHERS//////
-
+    void readPPM();
     //void readITD(); ? à mettre ici ?
 
 };
---
+++ src/Map.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include "../include/CatMonster.hpp"
+
+using namespace std;
+
+
+Map::~Map(){};
+
+//////////GETTERS//////////
+
+int Map::getHeight(){
+    return this->m_height;
+}
+
+int Map::getWidth(){
+    return this->m_width;
+}
+
+/////////SETTERS//////////
+
+void Map::setHeight(int newHeight){
+    this->m_height = newHeight; //Remplace valeur Height par une nouvelle
+}
+
+void Map::setWidth(int newWidth){
+    this->m_width=newWidth; //Remplace valeur Width par une nouvelle
+}
+
+//////////OTHER METHODS//////////
+
+void Map::Scale(int heightPPM, int widthPPM){
+    if (HeightPPM!=0 && widthPPM!=0){
+        setheight(this->heightPPM*30); //donne hauteur de notre image de ref fois 30 
+        setWidth(this->widthPPM*30); //donne largeur de notre image de ref fois 30 
+    } else {
+        printf("The picture does not have a proper size")
+    }
+}
+
