2f828738771b771cf3fce6bd838d257cded19339
EvaBien <eva.benharira@gmail.com>
2019-05-31 01:45:45 +0200

    Add towers images and justcat model

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -18,7 +18,7 @@ Graph::~Graph(){};
 //////////OTHER METHODS//////////
 
 
-void Graph::readITD(char* filename){
+int* Graph::readITD(char* filename){
 
   // open the file to read
 	FILE *file = fopen(filename, "r");
@@ -29,6 +29,17 @@ void Graph::readITD(char* filename){
     return EXIT_FAILURE;
   }
 
+  char tabNode[nbNode];
+  //vérifie qu'on a ou non atteint la fin du document
+  reading = (fgets(tabNode, nbNode, file) != NULL);
+
+  while (reading)
+  {
+    if (tabNode[0] != '#'){
+      
+    }
+  }
+  
   //lire ligne par ligne donc entre chaque \n
   //s'il y a un # sur la ligne ==> ne pas la prendre en compte
   //SINON chercher char et lui associer triplet RVB
@@ -38,6 +49,7 @@ void Graph::readITD(char* filename){
   // ie tant que ligne droite récup info revient à sauter de 5 en 5 case d'un tableau
 
   fclose(file);
+  return
 }
 
 void Graph::initGraph(){
