5e536216045ab65bc4e3298a4d282fb131b886d3
Unknown <laurelenn-sangare@laposte.net>
2019-06-01 12:11:06 +0200

    -> to .

--- src/CatMonster.cpp
+++ src/CatMonster.cpp
@@ -118,12 +118,12 @@ Tile* CatMonster::chooseDestination(Map *m){
   Tile *current = this->getTile();
   Node* (list) = m->getListNodes();
   int i=0;
-  while (list[i]->getNext()!=nullptr){
-    if (list[i]->getTile() == *current){
-      int successor = list[i]->getSuccessor();
+  while (list[i].getNext()!=nullptr){
+    if (list[i].getTile() == *current){
+      int successor = list[i].getSuccessor();
       Node *next = list[successor];
       // Pour utilisation de Dijsktra -> Node next = dijsktra(list[i]);
-      return *(next->getTile());
+      return *next->getTile();
     }
     i++;
   }
